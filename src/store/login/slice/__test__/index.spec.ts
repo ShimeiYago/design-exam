82e1fef5c4498e584f105c9cbb3a12126b3368ab
ShimeiYago <harrisonhightower4@gmail.com>
2024-10-06 13:43:26 +0900

    Handle email with redux

--- src/store/login/slice/__test__/index.spec.ts
+++ src/store/login/slice/__test__/index.spec.ts
@@ -1,4 +1,4 @@
-import { loginReducer, loginSlice } from '..';
+import { loginReducer, loginSlice, setEmail } from '..';
 import { LoginState } from '../../model';
 
 const { login } = loginSlice.actions;
@@ -6,16 +6,23 @@ const { login } = loginSlice.actions;
 describe('login reducer', () => {
   const initialState: LoginState = {
     isloggedIn: false,
+    email: '',
   };
 
   it('should handle initial state', () => {
     expect(loginReducer(undefined, { type: 'unknown' })).toEqual({
       isloggedIn: false,
+      email: '',
     });
   });
 
   it('should handle login action', () => {
     const actual = loginReducer(initialState, login());
     expect(actual.isloggedIn).toBeTruthy();
+  });
+
+  it('should handle setEmail action', () => {
+    const actual = loginReducer(initialState, setEmail('example@example.com'));
+    expect(actual.email).toBe('example@example.com');
   });
 });
